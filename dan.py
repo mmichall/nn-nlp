e5ff7bf9b827ec1a128ac708cd9176fa7fd68a1d
michal <michal.perelkiewicz@gmail.com>
2019-04-11 21:15:44 +0200

    some refactoring

--- dan.py
+++ dan.py
@@ -13,21 +13,21 @@ from pprint import pprint
 device = torch.device('cuda:0' if (torch.cuda.is_available()) else 'cpu')
 pprint("is CUDA available: {} so running on {}".format(torch.cuda.is_available(), device))
 
-# defining fields and tokenize function
-tokenize = lambda x: x.split()
-text_field = Field(sequential=True, tokenize=tokenize, lower=True, pad_first=True)
+# defining Fields for data sets
+text_field = Field(sequential=True, tokenize=lambda x: x.split(), lower=True, pad_first=True)
 label_field = Field(sequential=True, lower=True, use_vocab=True, is_target=True, pad_first=True)
 
-# defining datafields
 train_valid_datafields = [("id", None), ("type", None), ("review", text_field), ("label", label_field)]
 test_datafields = [("id", None), ("type", None), ("review", text_field), ("label", label_field)]
 
-is_pos_or_neg = lambda example: example.label[0] != 'unsup'
+has_sentiment_filter = lambda example: example.label[0] != 'unsup'
+
 # creating datsets
-train_dataset, valid_dataset = TabularDataset.splits(
+train_data_set, valid_data_set = TabularDataset.splits(
     path="resources/data",
-    filter_pred=is_pos_or_neg,
-    train='imdb_train.csv', validation="imdb_test.csv",
+    train='imdb_train.csv',
+    validation="imdb_test.csv",
+    filter_pred=has_sentiment_filter,
     format='csv',
     skip_header=True,
     fields=train_valid_datafields)
@@ -35,20 +35,20 @@ train_dataset, valid_dataset = TabularDataset.splits(
 test_dataset = TabularDataset(
     path="resources/data/imdb_test.csv",
     format='csv',
-    filter_pred=is_pos_or_neg,
+    filter_pred=has_sentiment_filter,
     skip_header=True,
     fields=test_datafields)
 
 
-text_field.build_vocab(train_dataset, test_dataset, vectors='glove.6B.100d')
-label_field.build_vocab(train_dataset)
+text_field.build_vocab(train_data_set, test_dataset, vectors='glove.6B.100d')
+label_field.build_vocab(valid_data_set)
 
 # vocab
 vocab: Vocab = text_field.vocab
 
 # Bucket Iterator
 train_iter, val_iter = BucketIterator.splits(
-    (train_dataset, valid_dataset),
+    (train_data_set, valid_data_set),
     batch_sizes=(128, 128),
     device=device,  # if you want to use the GPU, specify the GPU number here
     sort_key=lambda x: len(x.text_field),
@@ -79,7 +79,7 @@ optimiser = torch.optim.Adam(model.parameters(), lr=0.001)
 
 
 for epoch in range(num_epochs):
-    #model.train()
+    model.train()
 
     hist = []
     pbar = tqdm(enumerate(train_iter))
@@ -124,7 +124,6 @@ for epoch in range(num_epochs):
         golden_preds = np.append(golden_preds, target.cpu())
         test_preds = np.hstack(test_preds)
         golden_preds = np.hstack(golden_preds)
-
 
     mse = ((test_preds - golden_preds) ** 2).mean()
     pprint('MSE: {:.4f}'.format(mse))
