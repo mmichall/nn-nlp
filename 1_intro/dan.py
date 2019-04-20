7444bd065fd090f0cca3b14547a05227687a60c5
michal <michal.perelkiewicz@gmail.com>
2019-04-20 17:21:32 +0200

    Add stopwords

--- 1_intro/dan.py
+++ 1_intro/dan.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+from nltk.corpus import stopwords
+nltk.download('stopwords')
 
 from torchtext.data import Field
 from torchtext.data import TabularDataset
@@ -11,15 +13,15 @@ from pprint import pprint
 
 import sys
 sys.path.append('..')
-from model.model import LSTM
+from model.model import LSTM, NBoW, DeepCBoW
 
 ''' Specify a device to work on (CPU / GPU) '''
 device = torch.device('cuda:0' if (torch.cuda.is_available()) else 'cpu')
 pprint("is CUDA available: {} so running on {}".format(torch.cuda.is_available(), device))
 
 ''' Defining Fields for the training and testing data sets '''
-text_field = Field(sequential=True, tokenize=lambda x: x.split(), lower=True, pad_first=True)
-label_field = Field(sequential=True, lower=True, use_vocab=True, is_target=True, pad_first=True)
+text_field = Field(sequential=True, tokenize=lambda x: x.split(), lower=True, pad_first=True, batch_first=True, stop_words=set(stopwords.words('english')))
+label_field = Field(sequential=True, lower=True, use_vocab=True, is_target=True, unk_token=None, pad_token=None, batch_first=True)
 
 ''' Create dicts to load data from the specified columns and pre-process them during the Field instructions '''
 train_valid_data_fields = [("id", None), ("type", None), ("review", text_field), ("label", label_field)]
@@ -51,9 +53,9 @@ text_field.build_vocab(train_data_set, test_dataset, vectors=vectors)
 label_field.build_vocab(valid_data_set)
 
 ''' Define Bucket Iterators '''
-train_iter, val_iter = BucketIterator.splits(
+train_iter, val_iter = Iterator.splits(
     (train_data_set, valid_data_set),
-    batch_sizes=(128, 128),
+    batch_sizes=(64, 64),
     device=device,
     sort_key=lambda x: len(x.text_field),
     sort_within_batch=False,
@@ -61,7 +63,7 @@ train_iter, val_iter = BucketIterator.splits(
 )
 
 test_iter = Iterator(test_dataset,
-                     batch_size=128,
+                     batch_size=64,
                      device=device,
                      sort=False,
                      sort_within_batch=False,
@@ -69,6 +71,7 @@ test_iter = Iterator(test_dataset,
 
 
 ''' Define model '''
+'''
 model = LSTM(vocab_size=len(text_field.vocab.stoi),
              embed_size=100,
              hidden_dim=180,
@@ -76,17 +79,46 @@ model = LSTM(vocab_size=len(text_field.vocab.stoi),
              output_dim=1,
              num_layers=1)
 
+'''
 ''' Set the vocabulary from the dataset to embedding layer '''
+
+'''
 model.embeddings.weight.data = text_field.vocab.vectors
 model.cuda()
 
+'''
 ''' Training phrase '''
+
+'''
+model = NBoW(vocab_size=len(text_field.vocab.stoi),
+             embed_size=100,
+             hid_size=200)
+'''
+
+model = DeepCBoW(nwords=len(text_field.vocab.stoi),
+                 ntags=1,
+                 nlayers=2,
+                 emb_size=100,
+                 hid_size=64)
+
+model.embeddings.weight.data = text_field.vocab.vectors
+model.cuda()
+
 num_epochs = 10
 loss_fn = torch.nn.MSELoss(size_average=True)
 optimiser = torch.optim.Adam(model.parameters(), lr=0.001)
+
+'''
+model.fit(data_loader=train_iter,
+          val_data_loader=test_iter,
+          num_epochs=num_epochs,
+          loss_fn=loss_fn,
+          optimiser=optimiser)
+'''
 
 model.fit(data_loader=train_iter,
           val_data_loader=test_iter,
           num_epochs=num_epochs,
           loss_fn=loss_fn,
           optimiser=optimiser)
+
--- model/model.py
+++ model/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn.functional import tanh
 from tqdm import tqdm
 import numpy as np
 from pprint import pprint
@@ -109,3 +110,169 @@ class LSTM(nn.Module):
 
             equals = np.sum(np.equal(test_preds.round(), golden_preds.round()))
             pprint('Acc: {:.4f}'.format(equals / test_preds.size))
+
+
+class NBoW(nn.Module):
+
+    def __init__(self, vocab_size, embed_size, hid_size):
+        super(NBoW, self).__init__()
+        self.vocab_size = vocab_size
+        self.embed_size = embed_size
+        self.hid_size = hid_size
+
+        self.embeddings = nn.Embedding(self.vocab_size, self.embed_size)
+        self.linear = nn.Linear(self.embed_size, self.hid_size)
+
+        nn.init.xavier_uniform_(self.linear.weight)
+        self.output_layer = nn.Linear(hid_size, 1)
+        # initialize the weights with xavier uniform (Glorot, X. & Bengio, Y. (2010))
+
+        nn.init.xavier_uniform_(self.output_layer.weight)
+
+    def forward(self, x):
+        embeds = self.embeddings(x)
+
+        emb_sum = torch.sum(embeds, dim=1) # size(emb_sum) = emb_size
+        #emb_sum = emb_sum.view(1, -1) # size(emb_sum) = 1 x emb_size
+
+        y_pred = tanh(self.linear(emb_sum))
+
+        return self.output_layer(y_pred)
+
+    def fit(self, data_loader, val_data_loader, num_epochs, loss_fn, optimiser):
+        for epoch in range(num_epochs):
+            # ??
+            self.train()
+
+            hist = []
+            pbar = tqdm(enumerate(data_loader))
+            for i, batch in pbar:
+                # Clear stored gradient
+                self.zero_grad()
+                text, target = batch.review, batch.label
+
+                # Forward pass
+                y_pred = self(text)
+
+                target = target.float().view(-1)
+                loss = loss_fn(y_pred, target)
+
+                hist.append(loss.item())
+
+                # Zero out gradient, else they will accumulate between epochs
+                optimiser.zero_grad()
+
+                # Backward pass
+                loss.backward()
+
+                # Update parameters
+                optimiser.step()
+
+                pbar.set_description('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs, np.average(hist)))
+
+            test_preds = []
+            golden_preds = []
+            for batch in tqdm(val_data_loader):
+                text, target = batch.review, batch.label
+
+                preds = self(text)
+                preds = preds.cpu().data.numpy()
+                # the actual outputs of the model are logits, so we need to pass these values to the sigmoid function
+                # preds = 1 / (1 + np.exp(-preds))
+                test_preds = np.append(test_preds, preds)
+                golden_preds = np.append(golden_preds, target.cpu())
+                test_preds = np.hstack(test_preds)
+                golden_preds = np.hstack(golden_preds)
+
+            pprint(test_preds.round())
+            pprint(golden_preds.round())
+
+            equals = np.sum(np.equal(test_preds.round(), golden_preds.round()))
+            pprint('Acc: {:.4f}'.format(equals / test_preds.size))
+
+
+
+class DeepCBoW(torch.nn.Module):
+    def __init__(self, nwords, ntags, nlayers, emb_size, hid_size):
+        super(DeepCBoW, self).__init__()
+
+        """ variables """
+        self.nlayers = nlayers
+
+        """ layers """
+        self.embeddings = nn.Embedding(nwords, emb_size)
+        # initialize the weights with xavier uniform (Glorot, X. & Bengio, Y. (2010))
+        nn.init.xavier_uniform_(self.embeddings.weight)
+
+        # add nlayers number of layers
+        self.linears = nn.ModuleList([
+                nn.Linear(emb_size if i == 0 else hid_size, hid_size) \
+                for i in range(nlayers)])
+        # initialize the weights with xavier uniform (Glorot, X. & Bengio, Y. (2010))
+        for i in range(nlayers):
+            nn.init.xavier_uniform_(self.linears[i].weight)
+
+        self.output_layer = nn.Linear(hid_size, ntags)
+        # initialize the weights with xavier uniform (Glorot, X. & Bengio, Y. (2010))
+        nn.init.xavier_uniform_(self.output_layer.weight)
+
+    def forward(self, words):
+        emb = self.embeddings(words)
+        emb_sum = torch.sum(emb, dim=1) # size(emb_sum) = emb_size
+        h = emb_sum # size(h) = 1 x emb_size
+        for i in range(self.nlayers):
+            h = torch.tanh(self.linears[i](h))
+        out = self.output_layer(h)
+        return out
+
+    def fit(self, data_loader, val_data_loader, num_epochs, loss_fn, optimiser):
+        for epoch in range(num_epochs):
+            # ??
+            self.train()
+
+            hist = []
+            pbar = tqdm(enumerate(data_loader))
+            for i, batch in pbar:
+                # Clear stored gradient
+                self.zero_grad()
+                text, target = batch.review, batch.label
+
+                # Forward pass
+                y_pred = self(text)
+
+                target = target.float().view(-1)
+                loss = loss_fn(y_pred, target)
+
+                hist.append(loss.item())
+
+                # Zero out gradient, else they will accumulate between epochs
+                optimiser.zero_grad()
+
+                # Backward pass
+                loss.backward()
+
+                # Update parameters
+                optimiser.step()
+
+                pbar.set_description('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs, np.average(hist)))
+
+            test_preds = []
+            golden_preds = []
+            for batch in tqdm(val_data_loader):
+                text, target = batch.review, batch.label
+
+                preds = self(text)
+                preds = preds.cpu().data.numpy()
+                # the actual outputs of the model are logits, so we need to pass these values to the sigmoid function
+                # preds = 1 / (1 + np.exp(-preds))
+                test_preds = np.append(test_preds, preds)
+                golden_preds = np.append(golden_preds, target.cpu())
+                test_preds = np.hstack(test_preds)
+                golden_preds = np.hstack(golden_preds)
+
+            pprint(test_preds.round())
+            pprint(golden_preds.round())
+
+            equals = np.sum(np.equal(test_preds.round(), golden_preds.round()))
+            pprint('Acc: {:.4f}'.format(equals / test_preds.size))
+
