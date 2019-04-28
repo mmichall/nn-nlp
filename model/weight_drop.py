f84e73ef01304005fde63a5d0bc0498cf87cc0d9
michal <michal.perelkiewicz@gmail.com>
2019-04-28 19:39:39 +0200

    cannot assign 'torch.cuda.FloatTensor' as parameter 'weight' fix

--- model/weight_drop.py
+++ model/weight_drop.py
@@ -28,7 +28,7 @@ class WeightDrop(torch.nn.Module):
             w = getattr(self.module, name_w)
             for k in self.module._parameters:
                 print(k)
-            # del self.module._parameters[name_w] ???
+            del self.module._parameters[name_w]
             self.module.register_parameter(name_w + '_raw', Parameter(w.data))
 
     def _setweights(self):
@@ -39,9 +39,9 @@ class WeightDrop(torch.nn.Module):
                 mask = torch.autograd.Variable(torch.ones(raw_w.size(0), 1))
                 if raw_w.is_cuda: mask = mask.cuda()
                 mask = torch.nn.functional.dropout(mask, p=self.dropout, training=True)
-                w = mask.expand_as(raw_w) * raw_w
+                w = torch.nn.Parameter(mask.expand_as(raw_w) * raw_w)
             else:
-                w = torch.nn.functional.dropout(raw_w, p=self.dropout, training=self.training)
+                w = torch.nn.Parameter(torch.nn.functional.dropout(raw_w, p=self.dropout, training=self.training))
             setattr(self.module, name_w, w)
 
     def forward(self, *args):
