9350ca192dbb9e48df78a9c71cccf597efe966b6
michal <michal.perelkiewicz@gmail.com>
2019-04-29 21:51:04 +0200

    Playing with WeightDrop: bias_hh_l0

--- model/model.py
+++ model/model.py
@@ -35,7 +35,7 @@ class LSTM(nn.Module):
                             bidirectional=self.bidirectional,
                             batch_first=True)
 
-        self.wdrnn = WeightDrop(self.lstm, ['weight_hh_l0'], dropout=0.95, variational=True)
+        self.wdrnn = WeightDrop(self.lstm, ['weight_hh_l0'], dropout=1., variational=True)
         self.wdrnn.cuda()
 
         for k in self.wdrnn.module._parameters:
