125a6eb9c065fa9b46ece02c7700e6379a75a9fc
michal <michal.perelkiewicz@gmail.com>
2019-04-29 21:14:56 +0200

    Playing with WeightDrop: bias_hh_l0

--- model/weight_drop.py
+++ model/weight_drop.py
@@ -36,6 +36,7 @@ class WeightDrop(torch.nn.Module):
             if self.variational:
                 mask = torch.ones(raw_w.size(0), 1)
                 if raw_w.is_cuda: mask = mask.cuda()
+                print(mask)
                 mask = torch.nn.functional.dropout(mask, p=self.dropout, training=True) * self.dropout
                 print(mask.expand_as(raw_w))
                 print(mask.expand_as(raw_w) * raw_w)
