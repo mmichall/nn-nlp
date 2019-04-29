516a3f4f5a8d4497378ad3a205f3ba9ce1b939d3
michal <michal.perelkiewicz@gmail.com>
2019-04-29 18:51:24 +0200

    Testing DropConnect on hidden-to-hidden weights: 0.5

--- model/model.py
+++ model/model.py
@@ -52,7 +52,7 @@ class LSTM(nn.Module):
         # shape of self.hidden: (a, b), where a and b both
         # have shape (num_layers, batch_size, hidden_dim).
 
-        wdrnn = WeightDrop(self.lstm, ['weight_hh_l0'], dropout=0.9)
+        wdrnn = WeightDrop(self.lstm, ['weight_hh_l0'], dropout=0.5)
         wdrnn.cuda()
 
         lstm_out, _ = wdrnn(embeds)
